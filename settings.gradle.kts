23bb25d08992dc9cb9009521a0b0564134f3c93a
Imbus <imbus64@protonmail.com>
2023-11-09 16:48:24 +0100

    Initial

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    // Apply the foojay-resolver plugin to allow automatic download of JDKs
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+}
+
+rootProject.name = "swing_demo"
+include("app", "lab2", "lab3")
