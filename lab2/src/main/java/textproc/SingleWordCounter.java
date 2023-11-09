b1e32a5946ed69fcc8c914dbc2c5c54b3ac17131
thugborean <145711002+thugborean@users.noreply.github.com>
2023-11-09 21:57:45 +0100

    lab2 main program

--- lab2/src/main/java/textproc/SingleWordCounter.java
+++ lab2/src/main/java/textproc/SingleWordCounter.java
@@ -1,5 +1,5 @@
-package textproc;
 
+// Läser av ett enstaka ord
 public class SingleWordCounter implements TextProcessor {
 	private String word;
 	private int n;
@@ -9,12 +9,14 @@ public class SingleWordCounter implements TextProcessor {
 		n = 0;
 	}
 
+	// Om w är samam som objektets ord, n++
 	public void process(String w) {
-		if (w == word) {
+		if (w.equals(word)) {
 			n++;
 		}
 	}
 
+	// Skriv ut mängden ord som är .equals word
 	public void report() {
 		System.out.println(word + ": " + n);
 	}
